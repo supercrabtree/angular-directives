1ac667585d44767edbd7d43865d1da3ab9a31f4c
George Crabtree <supercrabtree@googlemail.com>
2014-04-03 13:07:05 +1100

    added other directive template

--- triggeronlast.js
+++ triggeronlast.js
@@ -1,12 +1,31 @@
 'use strict';
 
-angular.module('ajfApp')
+angular.module('myApp')
   .directive('triggerOnLast', function () {
-    return function (scope, element, attrs) {
-      if (scope.$last) {
-        element.ready(function () {
-          element.parent().scope().onReady();
-        });
+    return {
+      restrict: 'A',
+      link: function (scope, element, attrs) {
+        if (scope.$last) {
+          element.ready(function () {
+            element.parent().scope().onReady();
+          });
+        }
+      }
+    };
+  })
+  .directive('someOtherDirective', function () {
+    return {
+      restrict: 'E',
+      scope: true,
+      link: function postLink(scope, element, attrs) {
+        function onReady() {
+          // do stuffs with the ng-repeat items
+        }
+        if (angular.isDefined(attrs.waitForLast)) {
+          scope.onReady = onReady;
+        } else {
+          onReady();
+        }
       }
     };
   });
